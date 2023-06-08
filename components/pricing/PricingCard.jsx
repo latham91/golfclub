6fe875fd865602cd9c72c13945eeeeef09b453c2
Aaron Latham <alatham7891@gmail.com>
2023-06-08 20:27:56 +0100

    Fixed spacing on pricingcard

--- components/pricing/PricingCard.jsx
+++ components/pricing/PricingCard.jsx
@@ -21,43 +21,47 @@ export default function PricingCard({
             selected ? "bg-slate-800/80" : ""
          }`}
       >
-         <div className="flex flex-col gap-5">
-            <h1
-               className={`${playfairDisplay.className} flex flex-row items-center justify-between text-2xl font-semibold`}
-            >
-               {name}
-               {popular && (
-                  <span className="px-4 py-1 text-base rounded-full bg-gradient-to-br from-teal-500 to-teal-700">
-                     Most Popular
-                  </span>
-               )}
-            </h1>
-            <p>{description}</p>
-            <ul className="flex flex-col gap-3 p-3 font-semibold bg-white/5 backdrop-blur-sm rounded-2xl">
-               {features.map((feature, index) => (
-                  <li
-                     key={index}
-                     className="flex flex-row items-center gap-3 text-base"
-                  >
-                     <TbCircleCheckFilled size={18} />{" "}
-                     {feature}
-                  </li>
-               ))}
-            </ul>
-            <div className="text-center">
-               <h2
-                  className={`${playfairDisplay.className} mb-1 text-5xl font-bold`}
+         <div className="flex flex-col h-full gap-5 justify-evenly">
+            <div className="flex flex-col gap-5">
+               <h1
+                  className={`${playfairDisplay.className} flex flex-row items-center justify-between text-2xl font-semibold`}
                >
-                  {price}
-               </h2>
-               <p>vat included.</p>
+                  {name}
+                  {popular && (
+                     <span className="px-4 py-1 text-base rounded-full bg-gradient-to-br from-teal-500 to-teal-700">
+                        Most Popular
+                     </span>
+                  )}
+               </h1>
+               <p>{description}</p>
+               <ul className="flex flex-col gap-3 p-3 font-semibold bg-white/5 backdrop-blur-sm rounded-2xl">
+                  {features.map((feature, index) => (
+                     <li
+                        key={index}
+                        className="flex flex-row items-center gap-3 text-base"
+                     >
+                        <TbCircleCheckFilled size={18} />{" "}
+                        {feature}
+                     </li>
+                  ))}
+               </ul>
             </div>
-            <div className="text-center">
-               <button
-                  className={`${playfairDisplay.className} px-10 py-3 font-semibold transition bg-gradient-to-br from-teal-500 to-teal-700 border-2 rounded-xl border-white/50 hover:from-teal-600 hover:to-teal-800`}
-               >
-                  Choose this plan
-               </button>
+            <div className="flex flex-col gap-5">
+               <div className="text-center">
+                  <h2
+                     className={`${playfairDisplay.className} mb-1 text-5xl font-bold`}
+                  >
+                     {price}
+                  </h2>
+                  <p>vat included.</p>
+               </div>
+               <div className="text-center">
+                  <button
+                     className={`${playfairDisplay.className} px-10 py-3 font-semibold transition bg-gradient-to-br from-teal-500 to-teal-700 border-2 rounded-xl border-white/50 hover:from-teal-600 hover:to-teal-800`}
+                  >
+                     Choose this plan
+                  </button>
+               </div>
             </div>
          </div>
       </div>
